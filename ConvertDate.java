7820d70a566b2b68d2ef03d9589ecf69ab03e130
Nikol <39650438+nikolst@users.noreply.github.com>
2021-02-08 16:44:29 +0200

    split()_stringMethod
    Write a program that takes a date string formatted as YYYY-MM-DD as input, then converts and outputs it as MM/DD/YYYY.
    
    For instance, the input 2007-07-21 will result in the following output 07/21/2007.

---
+++ ConvertDate.java
@@ -0,0 +1,14 @@
+package planet;
+
+import java.util.Scanner;
+
+public class ConvertDate {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String dateString = input.nextLine();
+        String[] parts = dateString.split("-");
+
+        System.out.println(parts[1] + "/" + parts[2] + "/" + parts[0]);
+    }
+}
