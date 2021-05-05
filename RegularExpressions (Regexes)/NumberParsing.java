bcc826e1c9f80c228d304ad30c0554a31f7c7ff5
Nikol <39650438+nikolst@users.noreply.github.com>
2021-05-05 09:11:02 +0300

    Update NumberParsing.java

--- RegularExpressions (Regexes)/NumberParsing.java
+++ RegularExpressions (Regexes)/NumberParsing.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String regex = "(0|(\\+?|\\-?)[0-9]+\\.(0|[0-9]*[1-9])|(\\+?|\\-?)[0-9]+\\,(0|[0-9]*[1-9])|(\\+?|\\-?)[1-9]+)";
+        //String regex = "(0|(\\+?|\\-?)[0-9]+\\.(0|[0-9]*[1-9])|(\\+?|\\-?)[0-9]+\\,(0|[0-9]*[1-9])|(\\+?|\\-?)[1-9]+)";
+        String regex = "(0|[+-]?[0-9]+[.,](0|[0-9]*[1-9])|[+-]?[1-9]+)";
 
         String number = scanner.nextLine();
         System.out.println(number.matches(regex) ? "YES" : "NO");
