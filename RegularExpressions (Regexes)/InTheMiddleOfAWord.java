7e7d167fd27b85d342f0d9b3d71b4a7bb4e81036
Nikol <39650438+nikolst@users.noreply.github.com>
2021-05-05 09:14:59 +0300

    Patterns and Matcher (Hyperskill)
    The first line of the input contains a sequence of letters.
    The second line of the input contains some text.
    Your task is to determine if any of these words contains this sequence of letters in the middle. The sequence cannot be located at the start or at the end of the word, only in the middle. If there is such a word, you should output "YES", otherwise output "NO". A word can contain only symbols of the English alphabet. You should ignore the case while searching for matches.
    
    Sample Input 1:
    Gramm
    Java is the most popular programming language
    Sample Output 1:
    YES
    Sample Input 2:
    Press
    Regular expressions is hard to read, isnt it?
    Sample Output 2:
    YES
    Sample Input 3:
    some
    Wow! How awesome is that!
    Sample Output 3:
    NO

---
+++ RegularExpressions (Regexes)/InTheMiddleOfAWord.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String part = scanner.nextLine();
+        String line = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("[a-zA-Z]+" + part + "[a-zA-Z]+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(line);
+        
+        if (matcher.find()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        
+    }
+}
