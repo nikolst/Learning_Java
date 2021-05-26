5bf51823cc611fbf43daed6602ea8672ab5c015f
Nikol <39650438+nikolst@users.noreply.github.com>
2021-05-26 14:23:03 +0300

    Generic methods (Hyperskill)
    Define and implement a generic static method invert that returns the invert version of an input array.
    You can invert an array in-place in the input array.
    
    Sample Input 1:
    **Integer
    10 6 2 3
    Sample Output 1:
    3 2 6 10

---
+++ GenericMethods/InvertAnArray.java
@@ -0,0 +1,11 @@
+class ArrayUtils {
+    // define invert method here
+    public static <T> T[] invert(T[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            T temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
+}
