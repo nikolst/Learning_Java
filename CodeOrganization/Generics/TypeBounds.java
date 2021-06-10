55454e37dd42f13c19650318df08f6a5618b1433
Nikol <39650438+nikolst@users.noreply.github.com>
2021-06-10 13:16:24 +0300

    Type Bounds (Hyperskill)
    Three classes are given:
    
    class Book {
    }
    
    class Brochure extends Book {
    
        private String name;
    
        Brochure(String name) {
            this.name = name;
        }
    
        String getName() {
            return name;
        }
    
        void setName(String name) {
            this.name = name;
        }
    }
    
    public class Main {
    
        public static void main(String[] args) {
            Brochure brochure = new Brochure("NewBrochure");
            Shelf<Brochure> shelf = new Shelf<>();
            shelf.setElement(brochure);
            System.out.println(shelf.getElement().getName());
        }
    }
    Write a generic Shelf class which will have a field, containing a type of object, and methods getElement() and setElement(). The first method has to return an object which is contained in Shelf (which is assigned to the field), the second one is to assign a passed object to the field. The class Shelf must accept only different types of books (subtypes of Book class)!

---
+++ CodeOrganization/Generics/TypeBounds.java
@@ -0,0 +1,12 @@
+class Shelf<T extends Book> {
+    
+    private T element;
+    
+    public T getElement() {
+        return element;
+    }
+    
+    public void setElement(T element) {
+        this.element = element;
+    }
+}
