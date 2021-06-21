961cad0e0c63b82856ad297256deb4e05dbe18d7
Nikol <39650438+nikolst@users.noreply.github.com>
2021-06-21 18:55:51 +0300

    Replacing characters (Hyperskill)
    You have been asked to read and check a student's essay about the Eiffel Tower. You've noticed some recurring spelling errors and decided to write a program to correct these mistakes throughout the whole text.
    
    Your program should make all of the following changes (in any context):
    
    Franse —> France
    Eifel tower —> Eiffel Tower
    19th —> XIXth
    20th —> XXth
    21st —> XXIst
    
    Sample Input 1:
    The Eifel tower is a wrought-iron lattice tower on the Champ de Mars in Paris, Franse.
    Sample Output 1:
    The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.

---
+++ RegularExpressions (Regexes)/CheckTheEssay.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class CheckTheEssay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        // MY CODE STARTS HERE -----> 
+        System.out.println(text
+            .replaceAll("Franse", "France")
+            .replaceAll("Eifel tower", "Eiffel Tower")
+            .replaceAll("19th", "XIXth")
+            .replaceAll("20th", "XXth")
+            .replaceAll("21st", "XXIst"));
+    }
+}
